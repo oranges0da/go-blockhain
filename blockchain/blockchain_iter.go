9f2512866379983fc6ce5eb5d629b1f031d73f38
Alex Ionitel <oranggsoda@gmail.com>
2022-05-24 00:32:50 -0400

    NewIter() and Next()

--- blockchain/blockchain_iter.go
+++ blockchain/blockchain_iter.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"fmt"
+
 	badger "github.com/dgraph-io/badger/v3"
 	"github.com/oranges0da/goblockchain/block"
 	"github.com/oranges0da/goblockchain/utils"
@@ -21,12 +23,30 @@ func (chain *Blockchain) NewIter() *BlockchainIter {
 }
 
 func (iter *BlockchainIter) Next() *block.Block {
-	var block *block.Block
+	var byte_block []byte
 
 	err := iter.db.View(func(txn *badger.Txn) error {
+		var inner_block []byte
+
 		item, err := txn.Get(iter.currentHash)
 		utils.Handle(err)
+
+		err = item.Value(func(val []byte) error {
+			inner_block := append(inner_block, val...)
+			fmt.Printf("%s\n", inner_block)
+
+			return nil
+		})
+
+		byte_block := inner_block
+		fmt.Printf("%s\n", byte_block)
+
+		return err
 	})
+
+	utils.Handle(err)
+
+	block := utils.ToBlock(byte_block)
 
 	return block
 }
--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/oranges0da/goblockchain/blockchain"
+	"github.com/oranges0da/goblockchain/utils"
+)
+
 func main() {
+	BlockChain, err := blockchain.New("asdas")
+	utils.Handle(err)
+
+	fmt.Printf("%s\n", BlockChain)
 }
