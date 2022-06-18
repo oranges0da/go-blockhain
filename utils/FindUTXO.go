84378db70a113ad76f32c8634bcf4390e3c6fbe8
Alex Ionitel <oranggsoda@gmail.com>
2022-06-18 18:36:53 -0400

    finsihed FindUTXO() finding unspent outputs for a specific address(addr)

--- utils/FindUTXO.go
+++ utils/FindUTXO.go
@@ -6,5 +6,13 @@ func FindUTXO(addr string) []transaction.TxOutput {
 	var UTXOs []transaction.TxOutput
 	unspentTxs := FindUnspentTxs(addr)
 
+	for _, tx := range unspentTxs {
+		for _, out := range tx.Outputs {
+			if out.OutCanUnlock(addr) {
+				UTXOs = append(UTXOs, out)
+			}
+		}
+	}
+
 	return UTXOs
 }
