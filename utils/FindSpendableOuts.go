9c31637ad348abb9f3fe2150d5af18bf97d50f3c
Alex Ionitel <oranggsoda@gmail.com>
2022-06-18 20:11:47 -0400

    finished FindSpendableOuts() for finding if an address has enough spendable outputs for a certain amount that they want to send

---
+++ utils/FindSpendableOuts.go
@@ -0,0 +1,27 @@
+package utils
+
+import "encoding/hex"
+
+func FindSpendableOuts(addr string, amt int) (int, map[string][]int) {
+	unspentOuts := make(map[string][]int)
+	unspentTxs := FindUnspentTxs(addr)
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTxs {
+		txId := hex.EncodeToString(tx.ID)
+
+		for outIdx, out := range tx.Outputs {
+			if out.PubKey == addr && accumulated < amt {
+				accumulated += out.Value
+				unspentOuts[txId] = append(unspentOuts[txId], outIdx)
+
+				if accumulated >= amt {
+					break Work
+				}
+			}
+		}
+	}
+
+	return accumulated, unspentOuts
+}
