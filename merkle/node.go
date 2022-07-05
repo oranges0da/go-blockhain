6918c076afeaba3bbe83fb6acb16585a82848169
Alex Ionitel <oranggsoda@gmail.com>
2022-07-04 20:31:29 -0400

    finished main Merkle tree and node logic, New() for both trees and nodes

--- merkle/node.go
+++ merkle/node.go
@@ -8,14 +8,15 @@ type Node struct {
 	Data  []byte // most likely hash of the two txs under this node
 }
 
-func NewNode(left, right *Node, data []byte) *Node {
+func NewNode(left, right *Node, tx []byte) *Node {
 	node := &Node{}
 
 	// if node is at bottom of tree
 	if left == nil && right == nil {
-		hash := sha256.Sum256(data)
+		hash := sha256.Sum256(tx)
 		node.Data = hash[:]
 	} else {
+		// hash the two prev txs whilst "traveling up the tree"
 		prevHashes := append(left.Data, right.Data...)
 		hash := sha256.Sum256(prevHashes)
 		node.Data = hash[:]
--- merkle/tree.go
+++ merkle/tree.go
@@ -4,6 +4,35 @@ type Tree struct {
 	RootNode *Node
 }
 
-func NewTree() *Tree {
+func NewTree(txs [][]byte) *Tree {
+	var nodes []Node
 
+	// if there is an odd number of nodes, concat last two txs
+	if len(txs)%2 != 0 {
+		txs = append(txs, txs[len(txs)-1])
+	}
+
+	// make simple array of all txs in node format
+	for _, tx := range txs {
+		node := NewNode(nil, nil, tx)
+		nodes = append(nodes, *node)
+	}
+
+	// go through and hash every tx, "traveling up the tree"
+	// and get root hash to put into block
+	for i := 0; i < len(txs)/2; i++ {
+		var level []Node
+
+		for j := 0; j < len(nodes); j += 2 {
+			node := NewNode(&nodes[j], &nodes[j+1], nil)
+			level = append(level, *node)
+		}
+
+		nodes = level
+	}
+
+	// tree only contains top hash (most important!)
+	tree := &Tree{&nodes[0]}
+
+	return tree
 }
