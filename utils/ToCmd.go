59b99b933ab912d3a3691e17da43ce95bf8e19bc
Alex Ionitel <oranggsoda@gmail.com>
2022-07-08 01:24:19 -0400

    new small helper func that prints cmd in string to terminal

---
+++ utils/ToCmd.go
@@ -0,0 +1,15 @@
+package utils
+
+import "fmt"
+
+func ToCmd(bytes []byte) string {
+	var cmd []byte
+
+	for _, b := range bytes {
+		if b != 0x0 {
+			cmd = append(cmd, b)
+		}
+	}
+
+	return fmt.Sprintf("%v", cmd)
+}
