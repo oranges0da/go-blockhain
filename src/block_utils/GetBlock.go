871378389b4e975d187c13efe70f5744ceb935c1
Alex Ionitel <oranggsoda@gmail.com>
2022-07-28 17:06:33 -0400

    finished GetBlock util func for getting specific block according to hash

---
+++ src/block_utils/GetBlock.go
@@ -0,0 +1,26 @@
+package block_utils
+
+import (
+	"github.com/oranges0da/goblockchain/src/db"
+	"github.com/oranges0da/goblockchain/src/model"
+	"github.com/xujiajun/nutsdb"
+)
+
+// get specifc block according to hash
+func GetBlock(hash []byte) (model.Block, error) {
+	var block model.Block
+
+	// open db
+	db, err := db.OpenDB()
+
+	err = db.View(func(tx *nutsdb.Tx) error {
+		if e, err := tx.Get("root", hash); err != nil {
+			return err
+		} else {
+			block = ToBlock(e.Value)
+			return nil
+		}
+	})
+
+	return block, err
+}
