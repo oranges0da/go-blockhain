915d77a40c623ef1fcb0f817564402e6d8c533b7
Alex Ionitel <oranggsoda@gmail.com>
2022-07-27 22:53:38 -0400

    finished send module, main package for sending data to other peers and requesting data from other peers

--- src/net/send.go
+++ src/net/send.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/oranges0da/goblockchain/src/chain"
 	"github.com/oranges0da/goblockchain/src/model"
 	"github.com/oranges0da/goblockchain/src/utils"
 )
@@ -45,10 +46,55 @@ func SendData(addr string, data []byte) {
 	}
 }
 
+// send block to peer
 func SendBlock(addr string, b *model.Block) {
-	data := Block{nodeAddr, utils.ToByte[*model.Block](b)}
+	data := Block{nodeAddr, utils.ToByte(b)}
 	payload := utils.ToByte[any](data)
 	request := append(CmdToBytes("block"), payload...)
 
 	SendData(addr, request)
+}
+
+// send transaction to peer
+func SendTx(addr string, tx *model.Transaction) {
+	data := Tx{nodeAddr, utils.ToByte(tx)}
+	payload := utils.ToByte(data)
+	request := append(CmdToBytes("tx"), payload...)
+
+	SendData(addr, request)
+}
+
+// send inv to peer
+func SendInv(address, kind string, items [][]byte) {
+	inventory := Inv{nodeAddr, kind, items}
+	payload := utils.ToByte(inventory)
+	request := append(CmdToBytes("inv"), payload...)
+
+	SendData(address, request)
+}
+
+// send version to peer
+func SendVersion(addr string, chain *chain.Blockchain) {
+	bestHeight := chain.BlockHeight
+	payload := utils.ToByte(Version{version, bestHeight, nodeAddr})
+
+	request := append(CmdToBytes("version"), payload...)
+
+	SendData(addr, request)
+}
+
+// request for specific peer to send most recent block
+func AskBlocks(addr string) {
+	payload := utils.ToByte(GetBlocks{addr})
+	request := append(CmdToBytes("getblocks"), payload...)
+
+	SendData(addr, request)
+}
+
+// request for data from peer
+func SendGetData(address, kind string, id []byte) {
+	payload := utils.ToByte(GetData{nodeAddr, kind, id})
+	request := append(CmdToBytes("getdata"), payload...)
+
+	SendData(address, request)
 }
